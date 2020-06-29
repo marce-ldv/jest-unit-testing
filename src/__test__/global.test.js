8be2000ea85ebbafc0d2954e1ced8791fa0a6fcd
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-06-29 13:05:42 -0300

    Add tests

---
+++ src/__test__/global.test.js
@@ -0,0 +1,30 @@
+// import { shallow } from 'enzyme';
+
+const text = 'Hola Mundo';
+const fruits = ['manzana', 'melon', 'banana'];
+
+test('Debe contener un texto', () => {
+  expect(text).toMatch(/Mundo/);
+});
+
+test('Tenemos una banana?', () => {
+    expect(fruits).toContain('banana');
+});
+
+test('Mayor que', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test('Boolean', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split('').reverse().join(''));
+};
+
+test('Probar un callback', () => {
+  reverseString('hola', (str) => {
+    expect(str).toBe('aloh');
+  });
+});
